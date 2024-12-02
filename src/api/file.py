6d07a18a8f0d2a5eb297d0b0f2623a2fef6e6dd4
aliosmankaya <aokaya16@gmail.com>
2024-12-02 23:21:56 +0300

    fix: updated global path

--- src/api/file.py
+++ src/api/file.py
@@ -7,14 +7,14 @@ from ..parser.file import Update, Delete
 router = APIRouter()
 
 
-path = f"{os.path.abspath(os.curdir)}/src/file"
+path = f"{os.path.abspath(os.curdir)}/src/file/"
 if not os.path.exists(path):
     os.makedirs(path)
 
 
 @router.post("/upload")
 def upload_service(file: UploadFile = File(...)):
-    file_path = f"src/file/{file.filename}"
+    file_path = path + file.filename
     with open(file_path, "wb") as buffer:
         buffer.write(file.file.read())
     return JSONResponse(content="Uploaded successfully", status_code=200)
@@ -22,20 +22,18 @@ def upload_service(file: UploadFile = File(...)):
 
 @router.get("/list")
 def list_service():
-    files_path = "src/file"
-    files = os.listdir(files_path)
+    files = os.listdir(path)
     return JSONResponse(content=files, status_code=200)
 
 
 @router.put("/update")
 def update_service(params: Update):
-    file_path = "src/file/"
-    os.rename(file_path + params.file_name, file_path + params.new_name)
+    os.rename(path + params.file_name, path + params.new_name)
     return JSONResponse(content="Updated successfully", status_code=200)
 
 
 @router.delete("/delete")
 def delete_service(params: Delete):
-    file_path = f"src/file/{params.file_name}"
+    file_path = path + params.file_name
     os.remove(file_path)
     return JSONResponse(content="Deleted successfully", status_code=200)
